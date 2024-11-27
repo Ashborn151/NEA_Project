aa8f389cfd80ddf23c380fa9ce7006df90e125bf
Ashborn151 <marc.rodrigues1511@gmail.com>
2024-11-27 14:49:06 +0000

    Changed button and scene

--- src/main/java/com/example/nea_project/HelloApplication.java
+++ src/main/java/com/example/nea_project/HelloApplication.java
@@ -1,20 +1,44 @@
 package com.example.nea_project;
 
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
+import java.util.ArrayList;
+import java.util.List;
 
 import java.io.IOException;
 
+
+
+
 public class HelloApplication extends Application {
+
+    Group firstGroup = new Group();
+
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
-        stage.setTitle("Start Game");
-        stage.setScene(scene);
+        Button startButton = new Button("Start Game");
+        startButton.setFont(new Font(70));
+        startButton.setTextFill(Color.BLACK);
+        firstGroup.getChildren().add(startButton);
+        Scene firstScene = new Scene(firstGroup, 1850, 900, Color.WHITE);
+        stage.setTitle("Game");
+        stage.setScene(firstScene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
